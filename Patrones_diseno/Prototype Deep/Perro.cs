edebaab08b73dc73c3a43cf130f50691d149bb13
Cristian Gonzalez <vrdr_cmgm@hotmail.com>
2020-03-15 22:03:40 -0500

    Creacion de proyecto de prototype deep para clonar todos los atributos de un objeto

---
+++ Patrones_diseno/Prototype Deep/Perro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Deep
+{
+    public class Perro : ICloneable
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public Detalles Rasgos { get; set; }
+
+        public object Clone()
+        {
+            Perro clonado = this.MemberwiseClone() as Perro;
+            Detalles detalles = new Detalles() { Raza = this.Rasgos.Raza, Color = this.Rasgos.Color };
+            clonado.Rasgos = detalles;
+
+            return clonado;
+        }
+    }
+
+    public class Detalles
+    {
+        public string Raza { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Patrones_diseno/Prototype Deep/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Deep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro oPerro = new Perro() { Nombre = "Lucas", Edad = 5 };
+            oPerro.Rasgos = new Detalles() {Raza="Labrador",Color="Cafe"};
+
+            Console.WriteLine($"Se crea el primer objeto perro: {oPerro.Nombre}, edad {oPerro.Edad}" +
+                                            $", raza {oPerro.Rasgos.Raza}, color {oPerro.Rasgos.Color}");
+
+            Perro oPerroClon = oPerro.Clone() as Perro;
+
+            Console.WriteLine($"Se crea el segundo objeto a partir del primero: {oPerroClon.Nombre}, edad {oPerroClon.Edad}" +
+                                            $", raza {oPerroClon.Rasgos.Raza}, color {oPerroClon.Rasgos.Color}");
+
+            oPerroClon.Rasgos.Raza = "Golden";
+            oPerroClon.Rasgos.Color = "Dorado";
+
+            Console.WriteLine($"Se modifican atributos del clon");
+
+            Console.WriteLine($"Se crea el primer objeto perro: {oPerro.Nombre}, edad {oPerro.Edad}" +
+                                            $", raza {oPerro.Rasgos.Raza}, color {oPerro.Rasgos.Color}");
+
+            Console.WriteLine($"Se crea el segundo objeto a partir del primero: {oPerroClon.Nombre}, edad {oPerroClon.Edad}" +
+                                            $", raza {oPerroClon.Rasgos.Raza}, color {oPerroClon.Rasgos.Color}");
+
+            Console.ReadLine();
+        }
+    }
+}
